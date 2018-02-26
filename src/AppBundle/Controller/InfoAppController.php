dee5c93c8978d56098e37fce0bba15adb6bfad63
AnaMaria Ioan <anamaria.ioan@emag.ro>
2018-02-26 16:42:49 +0200

    home part 2

---
+++ src/AppBundle/Controller/InfoAppController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anamaria.ioan
+ * Date: 2/2/2018
+ * Time: 11:37 AM
+ */
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class InfoAppController extends Controller
+{
+    /**
+     * @Route("/home")
+     */
+    public function showAction()
+    {
+        return $this->render('home/show.html.twig');
+    }
+
+}
